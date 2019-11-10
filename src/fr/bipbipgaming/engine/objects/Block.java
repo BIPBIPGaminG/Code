1731777862de8585c541b1df780d8d60422b0cd5
BIPBIPGaminG <53536865+BIPBIPGaminG@users.noreply.github.com>
2019-11-10 01:14:51 +0100

    truc

---
+++ src/fr/bipbipgaming/engine/objects/Block.java
@@ -0,0 +1,101 @@
+package fr.bipbipgaming.engine.objects;
+
+import fr.bipbipgaming.engine.graphics.Material;
+import fr.bipbipgaming.engine.graphics.Mesh;
+import fr.bipbipgaming.engine.graphics.Vertex;
+import fr.bipbipgaming.engine.math.Vector2f;
+import fr.bipbipgaming.engine.math.Vector3f;
+
+public class Block {
+	
+	private Vector3f position, rotation, scale;
+	private String path;
+	
+	public Block(String path, Vector3f position, Vector3f rotation, Vector3f scale) {
+		
+		this.path = path;
+		this.position = position;
+		this.rotation = rotation;
+		this.scale = scale;
+		
+	}
+	
+	public Mesh mesh = new Mesh(new Vertex[] {
+			//Back face
+			new Vertex(new Vector3f(-0.5f,  0.5f, -0.5f), new Vector2f(0.0f, 0.0f)),
+			new Vertex(new Vector3f(-0.5f, -0.5f, -0.5f), new Vector2f(0.0f, 1.0f)),
+			new Vertex(new Vector3f( 0.5f, -0.5f, -0.5f), new Vector2f(1.0f, 1.0f)),
+			new Vertex(new Vector3f( 0.5f,  0.5f, -0.5f), new Vector2f(1.0f, 0.0f)),
+			
+			//Front face
+			new Vertex(new Vector3f(-0.5f,  0.5f,  0.5f), new Vector2f(0.0f, 0.0f)),
+			new Vertex(new Vector3f(-0.5f, -0.5f,  0.5f), new Vector2f(0.0f, 1.0f)),
+			new Vertex(new Vector3f( 0.5f, -0.5f,  0.5f), new Vector2f(1.0f, 1.0f)),
+			new Vertex(new Vector3f( 0.5f,  0.5f,  0.5f), new Vector2f(1.0f, 0.0f)),
+			
+			//Right face
+			new Vertex(new Vector3f( 0.5f,  0.5f, -0.5f), new Vector2f(0.0f, 0.0f)),
+			new Vertex(new Vector3f( 0.5f, -0.5f, -0.5f), new Vector2f(0.0f, 1.0f)),
+			new Vertex(new Vector3f( 0.5f, -0.5f,  0.5f), new Vector2f(1.0f, 1.0f)),
+			new Vertex(new Vector3f( 0.5f,  0.5f,  0.5f), new Vector2f(1.0f, 0.0f)),
+			
+			//Left face
+			new Vertex(new Vector3f(-0.5f,  0.5f, -0.5f), new Vector2f(0.0f, 0.0f)),
+			new Vertex(new Vector3f(-0.5f, -0.5f, -0.5f), new Vector2f(0.0f, 1.0f)),
+			new Vertex(new Vector3f(-0.5f, -0.5f,  0.5f), new Vector2f(1.0f, 1.0f)),
+			new Vertex(new Vector3f(-0.5f,  0.5f,  0.5f), new Vector2f(1.0f, 0.0f)),
+			
+			//Top face
+			new Vertex(new Vector3f(-0.5f,  0.5f,  0.5f), new Vector2f(0.0f, 0.0f)),
+			new Vertex(new Vector3f(-0.5f,  0.5f, -0.5f), new Vector2f(0.0f, 1.0f)),
+			new Vertex(new Vector3f( 0.5f,  0.5f, -0.5f), new Vector2f(1.0f, 1.0f)),
+			new Vertex(new Vector3f( 0.5f,  0.5f,  0.5f), new Vector2f(1.0f, 0.0f)),
+			
+			//Bottom face
+			new Vertex(new Vector3f(-0.5f, -0.5f,  0.5f), new Vector2f(0.0f, 0.0f)),
+			new Vertex(new Vector3f(-0.5f, -0.5f, -0.5f), new Vector2f(0.0f, 1.0f)),
+			new Vertex(new Vector3f( 0.5f, -0.5f, -0.5f), new Vector2f(1.0f, 1.0f)),
+			new Vertex(new Vector3f( 0.5f, -0.5f,  0.5f), new Vector2f(1.0f, 0.0f)),
+	}, new int[] {
+			//Back face
+			0, 1, 3,	
+			3, 1, 2,	
+			
+			//Front face
+			4, 5, 7,
+			7, 5, 6,
+			
+			//Right face
+			8, 9, 11,
+			11, 9, 10,
+			
+			//Left face
+			12, 13, 15,
+			15, 13, 14,
+			
+			//Top face
+			16, 17, 19,
+			19, 17, 18,
+			
+			//Bottom face
+			20, 21, 23,
+			23, 21, 22
+	}, new Material(path));
+	
+	public GameObject create() {
+		
+		System.out.println(path + "    1");
+		
+		mesh.create();
+		
+		return new GameObject(position, rotation, scale, mesh);
+		
+	}
+	
+	public void destroy() {
+		
+		mesh.destroy();
+		
+	}
+	
+}
